4d2173e45f75882ec4673874e30a73a8a41bf64d
tiagoantunes13 <tiagoantunes9991@gmail.com>
2021-11-11 19:15:50 +0000

    index

---
+++ src/main/java/tiagoantunes/recipe/sf5recipe/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package tiagoantunes.recipe.sf5recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"index",",",""})
+    public String getIndexPage(){
+        return "index";
+    }
+
+}
