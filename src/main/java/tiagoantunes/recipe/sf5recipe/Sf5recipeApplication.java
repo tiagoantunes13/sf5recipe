4f0190c6674c929c11356805c488030c8594f1df
tiagoantunes13 <tiagoantunes9991@gmail.com>
2021-11-11 19:00:36 +0000

    1

---
+++ src/main/java/tiagoantunes/recipe/sf5recipe/Sf5recipeApplication.java
@@ -0,0 +1,13 @@
+package tiagoantunes.recipe.sf5recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf5recipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sf5recipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tiagoantunes/recipe/sf5recipe/Sf5recipeApplicationTests.java
@@ -0,0 +1,13 @@
+package tiagoantunes.recipe.sf5recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sf5recipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
